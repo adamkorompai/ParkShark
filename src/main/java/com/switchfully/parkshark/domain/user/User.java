5e4f19f3b30d0c7934dce9fcb16c882022e890e7
Andrea Basili <andrea.basili@live.it>
2025-05-08 14:25:29 +0200

    define user class

--- src/main/java/com/switchfully/parkshark/domain/user/User.java
+++ src/main/java/com/switchfully/parkshark/domain/user/User.java
@@ -1,6 +1,9 @@
 package com.switchfully.parkshark.domain.user;
 
+import com.switchfully.parkshark.domain.license_plate.LicensePlate;
+import com.switchfully.parkshark.domain.membership.MembershipLevel;
 import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails;
 
 import java.time.LocalDateTime;
 
@@ -13,25 +16,97 @@ public class User {
     @SequenceGenerator(name = "user_seq", sequenceName = "user_seq", allocationSize = 1)
     private Long id;
 
-    @Column(name = "first_name")
+    @Column(name = "first_name", nullable = false, length = 50)
     private String firstName;
 
-    @Column(name = "last_name")
+    @Column(name = "last_name", nullable = false, length = 50)
     private String lastName;
 
-    @Column(name = "email")
+    @Column(name = "email", nullable = false, length = 50)
     private String email;
 
-    @Column(name = "telephone_number")
+    @Column(name = "telephone_number", length = 50)
     private String telephoneNumber;
 
-    @Column(name = "mobile_number")
+    @Column(name = "mobile_number", length = 50)
     private String mobileNumber;
 
-    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false, length = 50)
     private UserRole role;
 
     @Column(name = "registration_date", columnDefinition = "TIMESTAMP")
     private LocalDateTime registrationDate;
 
+//    @ManyToOne(optional = false)
+//    @JoinColumn(name = "address_id", referencedColumn = "id")
+//    private Address address;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "plate_number", referencedColumnName = "plate_number")
+    private LicensePlate licensePlate;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "membership_level", referencedColumnName = "name")
+    private MembershipLevel membershipLevel;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String email, String telephoneNumber, String mobileNumber, UserRole role, LocalDateTime registrationDate, LicensePlate licensePlate, MembershipLevel membershipLevel) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.telephoneNumber = telephoneNumber;
+        this.mobileNumber = mobileNumber;
+        this.role = role;
+        this.registrationDate = registrationDate;
+        this.licensePlate = licensePlate;
+        this.membershipLevel = membershipLevel;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public LicensePlate getLicensePlate() {
+        return licensePlate;
+    }
+
+    public MembershipLevel getMembershipLevel() {
+        return membershipLevel;
+    }
+
+    @Override
+    public String toString() {
+        return "User " + id + " " + firstName + " " + lastName + " " + email;
+    }
 }
