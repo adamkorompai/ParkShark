4729353ab00fd09e73395960514ce3e384250d86
Andrea Basili <andrea.basili@live.it>
2025-05-08 11:53:34 +0200

    define user rola enum

---
+++ src/main/java/com/switchfully/parkshark/domain/user/User.java
@@ -0,0 +1,14 @@
+package com.switchfully.parkshark.domain.user;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    @SequenceGenerator(name = "user_seq", sequenceName = "user_seq", allocationSize = 1)
+    private Long id;
+
+
+}
