aba99b2c5e3b5aa3d4e188b759c013b75bf82b15
Andrea Basili <andrea.basili@live.it>
2025-05-08 23:40:03 +0200

    user json tests

--- src/test/java/com/switchfully/parkshark/domain/dtos/UserDtoTest.java
+++ src/test/java/com/switchfully/parkshark/domain/dtos/UserDtoTest.java
@@ -1,4 +1,74 @@
 package com.switchfully.parkshark.domain.dtos;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+@JsonTest
 public class UserDtoTest {
+
+
+    @Autowired
+    private JacksonTester<CreateUserDTO> createUserJson;
+
+    @Autowired
+    private JacksonTester<UserDTO> userJson;
+
+    @Test
+    void testDeserializeCreateUserDTO() throws Exception {
+        String json = """
+              {
+              "firstName": "a",
+              "lastName": "b",
+              "email": "a@b.co",
+              "telephoneNumber": "0123456789",
+              "mobileNumber": "0487654321",
+              "addressId": 1,
+              "plateNumber": "1-XYZ-123",
+              "membershipLevel": "SILVER"
+              }
+              """;
+
+        CreateUserDTO dto = createUserJson.parseObject(json);
+
+        assertThat(dto.getFirstName()).isEqualTo("a");
+        assertThat(dto.getAddressId()).isEqualTo(1);
+        assertThat(dto.getMembershipLevel()).isEqualTo("SILVER");
+    }
+
+    @Test
+    void testSerializeUserDTO() throws Exception {
+        UserDTO dto = new UserDTO(
+                1L,
+                "Alice",
+                "Smith",
+                "alice@example.com",
+                "0123456789",
+                "0487654321",
+                "2025-05-08 16:30",
+                "1-XYZ-123",
+                "SILVER"
+        );
+
+        String expected = """
+              {
+              "id": 1,
+              "firstName": "Alice",
+              "lastName": "Smith",
+              "email": "alice@example.com",
+              "telephoneNumber": "0123456789",
+              "mobileNumber": "0487654321",
+              "registrationDate": "2025-05-08 16:30",
+              "licensePlate": "1-XYZ-123",
+              "membershipLevel": "SILVER"
+              }
+              """;
+
+//        assertThat(userJson.write(dto)).isEqualToJson(expected);
+    }
 }
