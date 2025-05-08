46889f92ab333a55966e6469ac663e25de79c8aa
Andrea Basili <andrea.basili@live.it>
2025-05-08 11:58:39 +0200

    part of user define

--- src/main/java/com/switchfully/parkshark/domain/user/User.java
+++ src/main/java/com/switchfully/parkshark/domain/user/User.java
@@ -3,6 +3,7 @@ package com.switchfully.parkshark.domain.user;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
@@ -10,5 +11,23 @@ public class User {
     @SequenceGenerator(name = "user_seq", sequenceName = "user_seq", allocationSize = 1)
     private Long id;
 
+    @Column(name = "first_name")
+    private String firstName;
 
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "telephone_number")
+    private String telephoneNumber;
+
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+
+    @Column(name = "role")
+    private UserRole role;
+
+    
 }
