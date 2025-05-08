0206fb6feb6b6bc7478acf1bb1447319364e0b53
Andrea Basili <andrea.basili@live.it>
2025-05-08 15:54:15 +0200

    test repositories

--- src/test/java/com/switchfully/parkshark/repository/license_plate_repositories/CountryCodeRepositoryTest.java
+++ src/test/java/com/switchfully/parkshark/repository/license_plate_repositories/CountryCodeRepositoryTest.java
@@ -1,4 +1,22 @@
 package com.switchfully.parkshark.repository.license_plate_repositories;
 
+import com.switchfully.parkshark.domain.license_plate.CountryCode;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
 public class CountryCodeRepositoryTest {
+
+    @Autowired
+    private CountryCodeRepository countryCodeRepository;
+
+
+    @Test
+    void saveCountryCode() {
+        countryCodeRepository.save(new CountryCode("IT", "Italy"));
+    }
+
+    @Test
+    void findByCountryCode() {}
 }
--- src/test/java/com/switchfully/parkshark/repository/license_plate_repositories/LicensePlateReposipositoryTest.java
+++ src/test/java/com/switchfully/parkshark/repository/license_plate_repositories/LicensePlateReposipositoryTest.java
@@ -1,4 +1,19 @@
 package com.switchfully.parkshark.repository.license_plate_repositories;
 
+import com.switchfully.parkshark.domain.license_plate.CountryCode;
+import com.switchfully.parkshark.domain.license_plate.LicensePlate;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
 public class LicensePlateReposipositoryTest {
+
+    @Autowired
+    private LicensePlateRepository licensePlateRepository;
+
+    @Test
+    void saveLicensePlate() {
+        licensePlateRepository.save(new LicensePlate("1-ELF-456", new CountryCode("IT", "Italy")));
+    }
 }
